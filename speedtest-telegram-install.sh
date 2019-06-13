b83c58bd6e44ba7c0dca58d6839540c59ae60e66
joserf <joserodriguesfilho@gmail.com>
2019-06-13 07:59:05 -0300

    Update speedtest-telegram-install.sh

--- speedtest-telegram-install.sh
+++ speedtest-telegram-install.sh
@@ -22,11 +22,12 @@ if [ -x /usr/bin/apt-get ]; then
   sed -i "s/Server=127.0.0.1/Server=${ZABBIX_SERVER_IP}/" /etc/zabbix/zabbix_agentd.conf
   sed -i "s/ServerActive=127.0.0.1/ServerActive=${ZABBIX_SERVER_IP}/" /etc/zabbix/zabbix_agentd.conf
   HOSTNAME=`hostname` && sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
-  sed -i '270iUserParameter=data[*],cat /tmp/speedtest.txt | grep "Data:" | cut -d " " -f2' /etc/zabbix/zabbix_agentd.conf
-  sed -i '271iUserParameter=upload[*],cat /tmp/speedtest.txt | grep "Upload:" | cut -d " " -f2' /etc/zabbix/zabbix_agentd.conf
-  sed -i '272iUserParameter=download[*],cat /tmp/speedtest.txt | grep "Download:" | cut -d " " -f2' /etc/zabbix/zabbix_agentd.conf
-  sed -i '273iUserParameter=servidor[*],cat /tmp/speedtest.txt | grep "Hosted" | cut -c 11-' /etc/zabbix/zabbix_agentd.conf
-  sed -i '274iUserParameter=ping[*],cat /tmp/speedtest.txt | grep "Hosted" | cut -d " " -f7' /etc/zabbix/zabbix_agentd.conf
+  sed -i '270iUserParameter=wanip[*],cat /tmp/speedtest.txt | grep "Testing from" | cut -d"(" -f2 | cut -d")" -f1' /etc/zabbix/zabbix_agentd.conf
+  sed -i '271iUserParameter=data[*],cat /tmp/speedtest.txt | grep "Data:" | cut -d " " -f2' /etc/zabbix/zabbix_agentd.conf
+  sed -i '272iUserParameter=upload[*],cat /tmp/speedtest.txt | grep "Upload:" | cut -d " " -f2' /etc/zabbix/zabbix_agentd.conf
+  sed -i '273iUserParameter=download[*],cat /tmp/speedtest.txt | grep "Download:" | cut -d " " -f2' /etc/zabbix/zabbix_agentd.conf
+  sed -i '274iUserParameter=servidor[*],cat /tmp/speedtest.txt | grep "Hosted" | cut -c 11-' /etc/zabbix/zabbix_agentd.conf
+  sed -i '275iUserParameter=ping[*],cat /tmp/speedtest.txt | grep "Hosted" | cut -d ":" -f2 | cut -d" " -f2' /etc/zabbix/zabbix_agentd.conf
   ufw allow 10050/tcp
   /etc/init.d/zabbix-agent restart
   exit 0
